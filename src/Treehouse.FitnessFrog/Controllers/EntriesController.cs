7bdfeb693482907eb82b333dca5c094e0a51ee06
Elita Judjallo <elita@webreality.co.uk>
2019-05-28 16:46:46 +0100

    fitnes app

---
+++ src/Treehouse.FitnessFrog/Controllers/EntriesController.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web.Mvc;
+using Treehouse.FitnessFrog.Data;
+using Treehouse.FitnessFrog.Models;
+
+namespace Treehouse.FitnessFrog.Controllers
+{
+    public class EntriesController : Controller
+    {
+        private EntriesRepository _entriesRepository;
+
+        public EntriesController()
+        {
+            _entriesRepository = new EntriesRepository();
+        }
+
+        public ActionResult Index()
+        {
+            List<Entry> entries = _entriesRepository.GetEntries();
+
+            // Calculate the total activity.
+            double totalActivity = entries
+                .Where(e => e.Exclude == false)
+                .Sum(e => e.Duration);
+
+            // Determine the number of days that have entries.
+            int numberOfActiveDays = entries
+                .Select(e => e.Date)
+                .Distinct()
+                .Count();
+
+            ViewBag.TotalActivity = totalActivity;
+            ViewBag.AverageDailyActivity = (totalActivity / (double)numberOfActiveDays);
+
+            return View(entries);
+        }
+
+        public ActionResult Add()
+        {
+            var entry = new Entry()
+            {
+                Date = DateTime.Today
+            };
+
+            SetUpActivitiesSelectListItems();
+
+            return View(entry);
+        }
+
+        [HttpPost]
+       
+        public ActionResult Add(Entry entry)
+        {
+            ValidateEntry(entry);
+
+            if (ModelState.IsValid)
+            {
+                _entriesRepository.AddEntry(entry);
+
+                TempData["Message"] = "Your entry was successfully added!";
+
+                return RedirectToAction("Index");
+            }
+
+            SetUpActivitiesSelectListItems();
+
+            return View(entry);
+        }
+
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            // GET THE REQUEST ENTRY FROM THE REPOSITORY
+            Entry entry = _entriesRepository.GetEntry((int)id);
+
+
+            // RETURN A STATUS OF "NOT FOUND" IF THE ENTRY 
+            // WASN'T FOUND
+            if (entry == null)
+            {
+                return HttpNotFound();
+            }
+
+            SetUpActivitiesSelectListItems();
+            
+            // PASS THE ENTRY INTO THE VIEW
+            return View(entry);
+        }
+
+        [HttpPost]
+       
+        public ActionResult Edit(Entry entry)
+        { 
+           //VALIDATE THE ENTRY
+            ValidateEntry(entry);
+
+            if (ModelState.IsValid)
+            {
+                _entriesRepository.UpdateEntry(entry);
+
+                TempData["Message"] = "Your entry was successfully updated!";
+
+                return RedirectToAction("Index");
+            }
+
+            SetUpActivitiesSelectListItems();
+
+            return View(entry);
+        }
+
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            var entry = _entriesRepository.GetEntry((int)id);
+
+            if (entry == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(entry);
+        }
+
+        [HttpPost]
+        
+        public ActionResult Delete(int id)
+        {
+            _entriesRepository.DeleteEntry(id);
+
+            TempData["Message"] = "Your entry was successfully deleted!";
+
+            return RedirectToAction("Index");
+        }
+
+        private void ValidateEntry(Entry entry)
+        {
+            if (ModelState.IsValidField("Duration") && entry.Duration <= 0)
+            {
+                ModelState.AddModelError("Duration",
+                    "The Duration field value must be greater than '0'.");
+            }
+        }
+
+        private void SetUpActivitiesSelectListItems()
+        {
+            ViewBag.ActivitiesSelectListItems = new SelectList(
+                Data.Data.Activities, "Id", "Name");
+        }
+    }
+}
